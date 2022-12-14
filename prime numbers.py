5f3e04f88b5d50c32750dc9b8567cf3520ca420b
Vinodsmiley8 <78037224+Vinodsmiley8@users.noreply.github.com>
2022-12-14 23:51:38 +0530

    Add files via upload

---
+++ prime numbers.py
@@ -0,0 +1,14 @@
+prime=[]
+lower_value = int(input ("Please, Enter the Lowest Range Value: "))  
+upper_value = int(input ("Please, Enter the Upper Range Value: "))  
+ 
+for number in range (lower_value, upper_value + 1):  
+    if number > 1:  
+        for i in range (2, number):  
+            if (number % i) == 0:  
+                break  
+        else:  
+            prime.append(number) 
+
+  
+print ("The Prime Numbers in the range are: \n",prime) 
